36856d77d48145867d211ce4e23f46b04cd1527a
louloudev59 <106763258+louloudev59@users.noreply.github.com>
2023-10-23 22:24:01 +0000

    rajout du script py

---
+++ projet débutant/1. Programme de calcul/solution/programme.py
@@ -0,0 +1,8 @@
+# Voici un exemple de code répondant à la consigne
+
+premiere_valeur = int(input("Choisissez un premier nombre : "))
+deuxieme_valeur = int(input("Choisissez une deuxième valeur : "))
+
+resultat = premiere_valeur * deuxieme_valeur
+
+print(f"{premiere_valeur} x {deuxieme_valeur} = {resultat}")
